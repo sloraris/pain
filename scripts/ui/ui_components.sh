28964b27aaff8fab51077fa264d8cfe89de1b93f
sloraris <97694636+sloraris@users.noreply.github.com>
2025-05-23 16:00:01 -0600

    updated ui components file, added splash screen for vanity

---
+++ scripts/ui/ui_components.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ Copyright (C) 2025 Parker Owings <sloraris@sloraris.dev>              ┃
+# ┃                                                                       ┃
+# ┃ UI heavily inspired by Dominik Willner's KIAUH <th33xitus@gmail.com>  ┃
+# ┃ https://github.com/dw-0/kiauh                                         ┃
+# ┃                                                                       ┃
+# ┃ This file is part of PAIN - Puppet Assisted Installation Navigator    ┃
+# ┃ https://github.com/sloraris/pain                                      ┃
+# ┃                                                                       ┃
+# ┃ This file may be distributed under the terms of the GNU GPLv3 license ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+set -e
+
+#===================================================#
+#================== FRAMEWORK PARTS ================#
+#===================================================#
+function top_bar() {
+    echo -e "${WHITE}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓${NC}"
+}
+
+function bottom_bar() {
+    echo -e "${WHITE}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛${NC}"
+}
+
+function hr() {
+    echo -e "${WHITE}┃━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┃${NC}"
+}
+
+function er() {
+    echo -e "${WHITE}┃                                                                       ┃${NC}"
+}
+
+#===================================================#
+#============= TITLE, CREDITS, & VERSION ===========#
+#===================================================#
+function logo() {
+
+    echo -e  "${WHITE}┃                ${PURPLE}██████╗     █████╗    ██╗   ███╗   ██╗${WHITE}                 ┃${NC}"
+    echo -e  "${WHITE}┃                ${PURPLE}██╔══██╗   ██╔══██╗   ██║   ████╗  ██║${WHITE}                 ┃${NC}"
+    echo -e  "${WHITE}┃                ${PURPLE}██████╔╝   ███████║   ██║   ██╔██╗ ██║${WHITE}                 ┃${NC}"
+    echo -e  "${WHITE}┃                ${PURPLE}██╔═══╝    ██╔══██║   ██║   ██║╚██╗██║${WHITE}                 ┃${NC}"
+    echo -e  "${WHITE}┃                ${PURPLE}██║        ██║  ██║   ██║   ██║ ╚████║${WHITE}                 ┃${NC}"
+    echo -e  "${WHITE}┃                ${PURPLE}╚═╝        ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═══╝${WHITE}                 ┃${NC}"
+    echo -e  "${WHITE}┃                Puppet Assisted Installation Navigator                 ┃${NC}"
+}
+
+function credits() {
+
+    echo -e  "${WHITE}┃                           ${PURPLE}A tool by sloraris${WHITE}                          ┃${NC}"
+}
+
+function version() {
+
+    echo -e "${WHITE}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┃ ${CYAN}${PAIN_VERSION_FORMATTED}${WHITE} ┃━━┛${NC}"
+}
+
+function title() {
+    # ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓ # top_bar
+    # ┃                                                                       ┃ # er
+    # ┃                ██████╗     █████╗    ██╗   ███╗   ██╗                 ┃ # logo
+    # ┃                ██╔══██╗   ██╔══██╗   ██║   ████╗  ██║                 ┃ #
+    # ┃                ██████╔╝   ███████║   ██║   ██╔██╗ ██║                 ┃ #
+    # ┃                ██╔═══╝    ██╔══██║   ██║   ██║╚██╗██║                 ┃ #
+    # ┃                ██║        ██║  ██║   ██║   ██║ ╚████║                 ┃ #
+    # ┃                ╚═╝        ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═══╝                 ┃ #
+    # ┃                Puppet Assisted Installation Navigator                 ┃ # logo
+    # ┃                                                                       ┃ # er
+    # ┃                           A tool by sloraris                          ┃ # credits
+    # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┃ v************* ┃━━┛ # version
+
+    top_bar
+    er
+    logo
+    er
+    credits
+    version
+}
---
+++ scripts/ui/ui_splash.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ Copyright (C) 2025 Parker Owings <sloraris@sloraris.dev>              ┃
+# ┃                                                                       ┃
+# ┃ UI heavily inspired by Dominik Willner's KIAUH <th33xitus@gmail.com>  ┃
+# ┃ https://github.com/dw-0/kiauh                                         ┃
+# ┃                                                                       ┃
+# ┃ This file is part of PAIN - Puppet Assisted Installation Navigator    ┃
+# ┃ https://github.com/sloraris/pain                                      ┃
+# ┃                                                                       ┃
+# ┃ This file may be distributed under the terms of the GNU GPLv3 license ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+set -e
+
+function splash_screen() {
+    local colors=("\033[1;31m" "\033[1;33m" "\033[1;32m" "\033[1;36m" "\033[1;34m" "\033[1;35m")
+    local logo=(
+        "                ██████╗     █████╗    ██╗   ███╗   ██╗"
+        "                ██╔══██╗   ██╔══██╗   ██║   ████╗  ██║"
+        "                ██████╔╝   ███████║   ██║   ██╔██╗ ██║"
+        "                ██╔═══╝    ██╔══██║   ██║   ██║╚██╗██║"
+        "                ██║        ██║  ██║   ██║   ██║ ╚████║"
+        "                ╚═╝        ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═══╝"
+    )
+
+    clear
+
+    # Animate each line of the logo
+    for i in {1..3}; do  # Number of color cycles
+        for ((j=0; j<${#colors[@]}; j++)); do
+            local color="${colors[j]}"
+            top_bar
+            er
+            for line in "${logo[@]}"; do
+                echo -e "${WHITE}┃${color}${line}${WHITE}                 ┃${NC}"
+            done
+            er
+            echo -e "${WHITE}┃                Puppet Assisted Installation Navigator                 ┃${NC}"
+            er
+            credits
+            bottom_bar
+            echo -en "\033[13A"
+            sleep 0.1
+        done
+    done
+
+    sleep 0.5
+    clear
+}
